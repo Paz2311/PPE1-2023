2a1a3af535780f9c606cbc598bee91a6f65e2521
Paz2311 <maria.paz2311@gmail.com>
2023-11-12 19:21:20 +0100

    ajout premier exercice s7

---
+++ Exercices/Exos7seance/Exercice1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+documento=$1
+linenum=0
+
+while read -r line
+do
+    echo "$line" | tr '[:upper:]' '[:lower:]'| tr -cd '[:alnum:] [:space:]'| grep -o '\w*' >> doclimpio.txt
+    linenum=$(expr $linenum + 1)
+done < "$documento"
+
